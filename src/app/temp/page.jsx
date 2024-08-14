940971bddb11fd4e78c222d00c21d5eb796b0e34
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-08-13 21:50:02 -0300

    aula 13-08 link

---
+++ src/app/temp/page.jsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+import styles from './page.module.css';
+
+export default function Temp() {
+    return(
+        <div className="containerGlobal">
+            <h1>Páginas acesso teste</h1>
+            <Link href={'/'}>
+                <span className={styles.linkTemp}>Home</span>
+            </Link>
+            <Link href={'/sobre'}>
+                <span className={styles.linkTemp}>Sobre</span>            
+            </Link>
+            <Link href={'/usuarios/cadastro'}>
+                <span className={styles.linkTemp}>Cadastro de usuário</span>
+            </Link>
+            <Link href={'/usuarios/login'}>
+                <span className={styles.linkTemp}>Login</span>
+            </Link>
+        </div>
+    );
+}
