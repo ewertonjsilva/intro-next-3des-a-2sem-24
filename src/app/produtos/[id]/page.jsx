147304d88dd31941f15faf9a97023ec155cb5f8e
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-09-16 20:43:25 -0300

    aula 16-09 - 01

--- src/app/produtos/[id]/page.jsx
+++ src/app/produtos/[id]/page.jsx
@@ -1,14 +1,16 @@
 
-import { produtos } from '../../../mocks/dados';
+// import { produtos } from '../../../mocks/dados';
 import Produto from '@/componentes/produtos/produto';
 
 export default function ProdutoID({ params }) {
 
-    const produto = produtos.filter((prd, index, array) => prd.prd_id === parseInt(params.id));
+    // const produto = produtos.filter((prd, index, array) => prd.prd_id === parseInt(params.id));
+    const idProduto = parseInt(params.id);
 
     return (
 
-        <Produto produto={produto[0]} />
+        // <Produto produto={produto[0]} />
+        <Produto idProduto={idProduto} />
 
     );
 }
--- src/componentes/produtos/produto/index.jsx
+++ src/componentes/produtos/produto/index.jsx
@@ -1,17 +1,43 @@
 'use client'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 
 import carr from '../../../../public/icones/carrinho.svg';
+import api from '@/services/api';
 
 import styles from './index.module.css';
 
 // import { produtos} from '../../../mocks/dados';
 
-function Produto({ produto }) {
+function Produto({ idProduto }) {
 
+    const [produto, setProduto] = useState([]);
     const [qtd, setQtd] = useState(1);
     const [total, setTotal] = useState(produto.prd_valor);
+
+    useEffect(() => {
+        handleCarregaProduto();
+
+        async function handleCarregaProduto() {
+            const dadosApi = {
+                prd_id: idProduto
+            }
+            try {
+                const response = await api.get('/produtos', dadosApi);                
+                const confirmaAcesso = response.data.sucesso;
+                if (confirmaAcesso) {
+                    const produtoApi = response.data.dados[0];
+                    setProduto(produtoApi);
+                }
+            } catch (error) {
+                if (error.response) {
+                    alert(error.response.data.mensagem + '\n' + error.response.data.dados);
+                } else {
+                    alert('Erro no front-end' + '\n' + error);
+                }
+            }
+        }
+    }, []);
 
     function handleAtlQtdVlr(nvVlr) {
         let totalTemp = 0;
@@ -19,39 +45,48 @@ function Produto({ produto }) {
         setQtd(Number(nvVlr));
         setTotal(totalTemp.toFixed(2));
     }
-
+console.log(produto.length);
     return (
         <div className={styles.container}>
-            <div className={styles.containerItem}>
-                <Image
-                    className={styles.imagemProd}
-                    src={produto.prd_img}
-                    alt={"Imagem " + produto.prd_nome}
-                />
-            </div>
-            <div className={styles.containerItem}>
-                <div className={styles.titulo}>
-                    <h1>{produto.prd_nome}</h1>
-                    <Image src={produto.img_tp_prod} className={styles.icon} alt={produto.img_tp_prod} />
-                </div>
-                <span className={styles.descricao}>{produto.prd_descricao}</span>
-                <span className={styles.valor}>{'R$ ' + produto.prd_valor}</span>
-                <div className={styles.comprar}>
-                    <span className={styles.spanQtd}>Quantidade</span>
-                    <input
-                        type="number" 
-                        className={styles.input}
-                        min={1}
-                        onChange={nvVlr => handleAtlQtdVlr(nvVlr.target.value)}
-                        value={qtd}
-                    />
-                    <span className={styles.spanTt}>Total R$ {total}</span>
-                    <button className={styles.button} /*onClick={() => handleAddItem()}*/>                    
-                        <p className={styles.lblComp}>Inserir no carrinho</p>
-                        <Image className={styles.imgBtn} src={carr} alt="adicionar" />
-                    </button>
-                </div>
-            </div>
+            {
+                produto ?
+                    <>
+                        <div className={styles.containerItem}>
+                            {/* <Image
+                                className={styles.imagemProd}
+                                src={produto.prd_img}
+                                alt={"Imagem " + produto.prd_nome}
+                            /> */}
+                        </div>
+                        <div className={styles.containerItem}>
+                            <div className={styles.titulo}>
+                                <h1>{produto.prd_nome}</h1>
+                                {/* <Image src={produto.img_tp_prod} className={styles.icon} alt={produto.img_tp_prod} /> */}
+                            </div>
+                            <span className={styles.descricao}>{produto.prd_descricao}</span>
+                            <span className={styles.valor}>{'R$ ' + produto.prd_valor}</span>
+                            <div className={styles.comprar}>
+                                <span className={styles.spanQtd}>Quantidade</span>
+                                <input
+                                    type="number"
+                                    className={styles.input}
+                                    min={1}
+                                    onChange={nvVlr => handleAtlQtdVlr(nvVlr.target.value)}
+                                    value={qtd}
+                                />
+                                <span className={styles.spanTt}>Total R$ {total}</span>
+                                <button className={styles.button} /*onClick={() => handleAddItem()}*/>
+                                    <p className={styles.lblComp}>Inserir no carrinho</p>
+                                    <Image className={styles.imgBtn} src={carr} alt="adicionar" />
+                                </button>
+                            </div>
+                        </div>
+                    </>
+                    :
+                    <>
+                        <h1>Não há resultados para a requisição</h1>
+                    </>
+            }
         </div>
     );
 }
