2368752f2a69976125f094a9e956caf2d3b20d80
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-10-04 22:51:32 -0300

    aula 04-10 noite

--- src/componentes/carrinho/index.jsx
+++ src/componentes/carrinho/index.jsx
@@ -1,5 +1,6 @@
 'use client'
 import { useState, useEffect } from 'react';
+import Image from 'next/image';
 import { RiDeleteBin6Line, RiAddLine, RiSubtractLine } from "react-icons/ri";
 
 import styles from './index.module.css';
@@ -8,18 +9,18 @@ function CompCarrinho() {
 
   const [produtosCarrinho, setProdutosCarrinho] = useState([]);
 
-//   useEffect(() => {
-//     setProdutosCarrinho(location.state);
-//   }, []);
+  //   useEffect(() => {
+  //     setProdutosCarrinho(location.state);
+  //   }, []);
 
-//   // Use a função reduce para somar o valor total
-//   const valorTotal = produtos.reduce((total, produto) => {
-//     // Converta o campo prd_valor para um número (removendo o símbolo de moeda, se houver)
-//     const valor = parseFloat(produto.prd_valor.replace('$', '').replace(',', '.'));
+  //   // Use a função reduce para somar o valor total
+  //   const valorTotal = produtos.reduce((total, produto) => {
+  //     // Converta o campo prd_valor para um número (removendo o símbolo de moeda, se houver)
+  //     const valor = parseFloat(produto.prd_valor.replace('$', '').replace(',', '.'));
 
-//     // Multiplique a quantidade pelo valor e adicione ao total
-//     return total + produto.quantidade * valor;
-//   }, 0);
+  //     // Multiplique a quantidade pelo valor e adicione ao total
+  //     return total + produto.quantidade * valor;
+  //   }, 0);
 
   return (
     <div className={styles.centraliza}>
@@ -33,34 +34,44 @@ function CompCarrinho() {
       <div className={styles.grid}>
         <div className={styles.carrProduto}>
           <div className={styles.contImgCarrProd}>
-            {/* <img src={produtosCarrinho[0].prd_img} alt={produtosCarrinho[0].prd_nome} /> */}
-            <h2>imagem</h2>
+            <Image
+              src={'/temp/fritas.jpg'}
+              alt={'fritas'}
+              width={100}
+              height={100}
+              className={styles.imagemProduto}
+            />
           </div>
           <span>Nome Produto</span>
           <RiDeleteBin6Line />
         </div>
-        <div className={[styles.carrProduto, styles.carrQtd]}><RiSubtractLine />Qtd<RiAddLine /></div>
-        <div className={[styles.carrProduto, styles.valores]}>Valor</div>
-        <div className={[styles.carrProduto, styles.valores]}>R$ Total</div>
+        <div className={`${styles.carrProduto} ${styles.carrQtd}`}><RiSubtractLine />5<RiAddLine /></div>
+        <div className={`${styles.carrProduto} ${styles.valores}`}>5,00</div>
+        <div className={`${styles.carrProduto} ${styles.valores}`}>R$ 25,00</div>
       </div>
 
       <div className={styles.grid}>
         <div className={styles.carrProduto}>
           <div className={styles.contImgCarrProd}>
-            {/* <img src={produtosCarrinho[0].prd_img} alt={produtosCarrinho[0].prd_nome} /> */}
-            <h2>imagem</h2>
+            <Image
+              src={'/temp/sorvete.jpg'}
+              alt={'fritas'}
+              width={100}
+              height={100}
+              className={styles.imagemProduto}
+            />
           </div>
           <span>Nome Produto</span>
           <RiDeleteBin6Line />
         </div>
-        <div className={[styles.carrProduto, styles.carrQtd]}><RiSubtractLine />Qtd<RiAddLine /></div>
-        <div className={[styles.carrProduto, styles.valores]}>Valor</div>
-        <div className={[styles.carrProduto, styles.valores]}>R$ Total</div>
+        <div className={`${styles.carrProduto} ${styles.carrQtd}`}><RiSubtractLine />5<RiAddLine /></div>
+        <div className={`${styles.carrProduto} ${styles.valores}`}>5,00</div>
+        <div className={`${styles.carrProduto} ${styles.valores}`}>R$ 25,00</div>
       </div>
 
       <div className={styles.gridTotal}>
         <div></div>
-        <div className={styles.total}>R$ Total</div>
+        <div className={styles.total}>R$ 50,00</div>
       </div>
     </div>
   );
